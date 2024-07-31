7343727abb2a129dd60dab9b6fff98bf512e2d11
Vishwajeet.Sawant <vishwajeet.sawant@HOSMAC8523487.local>
2024-07-31 23:31:06 +0530

    updated UI

--- app_autoreply_groq.py
+++ app_autoreply_groq.py
@@ -51,25 +51,23 @@ with st.container():
 
 
 with st.container():
-    text_1, text_2 = st.columns((1, 2))
+    
+    with st.form(key='my_form', clear_on_submit=True):
+        d = {}
+        for i in req_list:
+            d["{0}".format(i)] = st.text_input(i)
 
-    with text_1:
-        with st.form(key='my_form', clear_on_submit=True):
-            d = {}
-            for i in req_list:
-                d["{0}".format(i)] = st.text_input(i)
+        submit_button = st.form_submit_button(label='Submit')
 
-            submit_button = st.form_submit_button(label='Submit')
+        if submit_button:
+            st.subheader('You entered the following details are:')
 
-            if submit_button:
-                st.subheader('You entered the following details are:')
+            dict_string = ', '.join(f'{key}: {value}' for key, value in d.items())
+            
+            system_2 = "you are a email replying assistant. you have to reply to the email regarding job requirement on behalf of the user. start by thanking the sender of the email. the email which is supposed to be replied is {email_txt}. the fields to be filled are given by the user."
+            human_2 = "{field_to_fill}"
+            prompt_2 = ChatPromptTemplate.from_messages([("system", system_2), ("human", human_2)])
 
-                dict_string = ', '.join(f'{key}: {value}' for key, value in d.items())
-                
-                system_2 = "you are a email replying assistant. you have to reply to the email regarding job requirement on behalf of the user. start by thanking the sender of the email. the email which is supposed to be replied is {email_txt}. the fields to be filled are given by the user."
-                human_2 = "{field_to_fill}"
-                prompt_2 = ChatPromptTemplate.from_messages([("system", system_2), ("human", human_2)])
+            chain_2 = prompt_2 | model  | output_parser
 
-                chain_2 = prompt_2 | model  | output_parser
-
-                st.write(chain_2.invoke({'email_txt': txt_input, 'field_to_fill': dict_string}))
+            st.write(chain_2.invoke({'email_txt': txt_input, 'field_to_fill': dict_string}))
